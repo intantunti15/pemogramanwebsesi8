2c89266e0242fa1b34f320cd61cefff6dc7ab537
intantunti15 <127914172+intantunti15@users.noreply.github.com>
2023-05-03 12:18:21 +0800

    Add files via upload

---
+++ intan2201010393/sesi8/User-new.php
@@ -0,0 +1,31 @@
+<h3>Tambah Data User</h3>
+<form method="POST" action="tbuser.php">
+    <input type="hidden" name="opsi" value="store">
+    <div>
+        Nama Lengkap
+        <input type="text" name="txNAMA">
+    </div>
+    <div>
+        Email
+        <input type="email" name="txEMAIL">
+    </div>
+    <div>
+        User Name
+        <input type="text" name="txUSERNAME">
+    </div>
+    <div>
+        password
+        <input type="password" name="txPASS1">
+    </div>
+    <div>
+        verifikasi password
+        <input type="password" name="txPASS2">
+    </div>
+
+    <button type="submit"> Buat Data User </button>
+
+
+
+
+
+</form>
